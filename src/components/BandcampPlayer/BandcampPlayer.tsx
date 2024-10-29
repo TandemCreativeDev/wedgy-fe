d9d126f1026627887c4d3c74ff91a6d553409bc6
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-29 20:28:52 +0000

    refactor: finish bandcamp component

--- src/components/BandcampPlayer/BandcampPlayer.tsx
+++ src/components/BandcampPlayer/BandcampPlayer.tsx
@@ -1,3 +1,17 @@
-export default function BandcampPlayer() {
-  return <h1></h1>;
+interface SongProps {
+  src: string;
+  title: string;
+  href: string;
+}
+
+export default function BandcampPlayer({ song }: { song: SongProps }) {
+  return (
+    <iframe
+      style={{ border: 0, width: "350px", height: "442px" }} // Use an object here
+      src={song.src}
+      seamless
+    >
+      <a href={song.href}>{song.title}</a>
+    </iframe>
+  );
 }
--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -1,47 +1,15 @@
 import Header from "../components/Header/Header";
 import BandcampPlayer from "../components/BandcampPlayer/BandcampPlayer";
+import bandCampData from "../assets/data/bandcamp.json";
 
 export default function Music() {
   return (
     <>
       <Header />
       <div className="flex flex-wrap gap-24 mt-24 w-10/12 m-auto mb-16">
-        <iframe
-          style={{ border: 0, width: "350px", height: "442px" }} // Use an object here
-          src="https://bandcamp.com/EmbeddedPlayer/track=1695960462/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/"
-          seamless
-        >
-          <a href="https://wedgefund.bandcamp.com/track/speak-to-me">
-            Speak To Me by Wedgy
-          </a>
-        </iframe>
-        <iframe
-          style={{ border: 0, width: "350px", height: "442px" }} // Use an object here
-          src="https://bandcamp.com/EmbeddedPlayer/track=463988426/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/"
-          seamless
-        >
-          <a href="https://wedgefund.bandcamp.com/track/stitch-the-lines">
-            Stitch The Lines by Wedgy
-          </a>
-        </iframe>
-        <iframe
-          style={{ border: 0, width: "350px", height: "442px" }} // Use an object here
-          src="https://bandcamp.com/EmbeddedPlayer/track=1520204842/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/"
-          seamless
-        >
-          <a href="https://wedgefund.bandcamp.com/track/wasnt-present-in-me">
-            Wasn't Present In Me by Wedgy
-          </a>
-        </iframe>
-        <iframe
-          style={{ border: 0, width: "350px", height: "470px" }} // Use an object here
-          src="https://bandcamp.com/EmbeddedPlayer/album=3432374493/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/"
-          seamless
-        >
-          <a href="https://wedgefund.bandcamp.com/album/redundant-in-rio">
-            Redundant in Rio by Wedgy
-          </a>
-        </iframe>
+        {bandCampData.map((song) => {
+          return <BandcampPlayer song={song} />;
+        })}
       </div>
     </>
   );

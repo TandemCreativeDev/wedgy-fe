78f1c187cfb546023be44babe41447e92c9384ff
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-29 13:57:01 +0000

    feat: home page looks like figma

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -3,20 +3,25 @@ const navItemClasses = "hover:text-gray-400";
 export default function Navbar() {
   return (
     <nav>
-      <ul className="flex space-x-4 list-none absolute">
+      <ul className="text-white text-xl font-lexend flex space-x-4 list-none absolute top-1/2  left-1/2 w-10/12 justify-between transform -translate-x-1/2 -translate-y-1/2">
         <li>
           <a href="/" className={navItemClasses}>
-            Home
+            TOUR
           </a>
         </li>
         <li>
           <a href="/about" className={navItemClasses}>
-            About
+            MUSIC
           </a>
         </li>
         <li>
           <a href="/contact" className={navItemClasses}>
-            Contact
+            MERCH
+          </a>
+        </li>
+        <li>
+          <a href="/contact" className={navItemClasses}>
+            CONTACT
           </a>
         </li>
       </ul>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,10 @@ import backgroundImg from "../assets/images/background.jpg";
 export default function Home() {
   return (
     <main>
-      <img className="absolute h-full" src={backgroundImg} alt="" />
+      <img className="-z-50 relative w-screen " src={backgroundImg} alt="" />
+      <h1 className="absolute top-8 left-1/2 transform -translate-x-1/2  text-white text-5xl font-kalnia">
+        Wedgy.
+      </h1>
     </main>
   );
 }

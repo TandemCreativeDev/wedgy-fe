62d2ab9e1c6e7c765beb654edc5a8a870da0f1f9
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-29 15:34:05 +0000

    feat: idek sorry max

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,11 +4,11 @@ export default function Home() {
   return (
     <main className="relative overflow-hidden w-full h-screen">
       <img
-        className="-z-50 absolute inset-0 object-cover w-full h-full object-right"
+        className="-z-50 absolute inset-0 object-cover w-full h-full "
         src={backgroundImg}
         alt=""
       />
-      <h1 className="absolute top-8 left-1/2 transform -translate-x-1/2 text-white text-5xl font-kalnia">
+      <h1 className="hidden absolute md:block top-8 left-1/2 transform -translate-x-1/2 text-white text-5xl font-kalnia">
         Wedgy.
       </h1>
     </main>

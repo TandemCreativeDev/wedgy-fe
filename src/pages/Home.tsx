e543f83eb7d15c60f3debec7af5ddfca96ed434b
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-29 14:53:26 +0000

    style: image now resize to the left

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,13 @@ import backgroundImg from "../assets/images/background.jpg";
 
 export default function Home() {
   return (
-    <main>
-      <img className="-z-50 relative w-screen " src={backgroundImg} alt="" />
-      <h1 className="absolute top-8 left-1/2 transform -translate-x-1/2  text-white text-5xl font-kalnia">
+    <main className="relative overflow-hidden w-full h-screen">
+      <img
+        className="-z-50 absolute inset-0 object-cover w-full h-full"
+        src={backgroundImg}
+        alt=""
+      />
+      <h1 className="absolute top-8 left-1/2 transform -translate-x-1/2 text-white text-5xl font-kalnia">
         Wedgy.
       </h1>
     </main>

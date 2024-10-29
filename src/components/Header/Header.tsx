adaee227ac1fd5f510c5cae5fa64f76bbd9e5309
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-29 16:45:39 +0000

    feat: add bandcamp embeds to music page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,14 @@
 // import Navbar from "./Navbar/Navbar";
 
 export default function Header() {
-  return <header>{/* <Navbar /> */}</header>;
+  return (
+    <header>
+      <a
+        href="/"
+        className="mt-7 ml-7 hidden md:block transform text-white text-5xl font-kalnia z-20 hover:text-gray-300"
+      >
+        Wedgy.
+      </a>
+    </header>
+  );
 }

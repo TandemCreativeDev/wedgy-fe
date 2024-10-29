5efe75e7e705335cff4fb75c8d528d609eb6bd8c
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-29 20:30:46 +0000

    style: change width of songs and make footer fixed to bottom

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-screen bg-gray-900 flex justify-center items-center h-12 text-white">
+    <footer className="w-screen fixed bottom-0 bg-gray-900 flex justify-center items-center h-12 text-white">
       <p>&copy; 2024 &nabla; Viatora Digital. All rights reserved.</p>
     </footer>
   );
--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -6,7 +6,7 @@ export default function Music() {
   return (
     <>
       <Header />
-      <div className="flex flex-wrap gap-24 mt-24 w-10/12 m-auto mb-16">
+      <div className="flex flex-wrap gap-24 mt-24 w-11/12 m-auto mb-16">
         {bandCampData.map((song) => {
           return <BandcampPlayer song={song} />;
         })}

33928f5c6d475506d4f9b25e5a7bf0f84f3083e1
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-30 14:15:16 +0000

    style: remove margin top from header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import MobileNav from "./Navbar/Navbar";
 
 export default function Header() {
   return (
-    <header className="w-full flex justify-center mt-5 md:block h-12">
+    <header className="w-full flex justify-center md:block h-12">
       <a
         href="/"
         className="md:mt-7 md:ml-7 block transform text-white text-5xl font-kalnia z-20 hover:text-gray-300"

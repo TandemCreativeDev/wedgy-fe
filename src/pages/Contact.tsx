56366ca2220d1523c6506132b7aeda28ffe02e15
maxitect <maxime.downe@gmail.com>
2024-11-02 17:54:46 +0000

    fix: gap between image and footer on multiple pages

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,7 @@
 export default function Contact() {
   return (
-    <main>
-      <h2 className=" font-lexend text-white text-5xl md:text-7xl text-center tracking-widest">
+    <main className="min-h-[calc(100vh-7rem)]">
+      <h2 className="font-lexend text-white text-5xl md:text-7xl text-center tracking-widest">
         CONTACT
       </h2>
       <p className="text-center mt-24 mb-24">wedgefundmusic@gmail.com</p>
